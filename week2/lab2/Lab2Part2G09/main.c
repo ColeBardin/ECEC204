63e3fd5fe212a69cd50ae7ae4aaf62d8ad72921f
ColeBardin <cab572@drexel.edu>
2022-07-01 13:31:43 -0400

    lab2 part2

--- week2/lab2/Lab2Part2G09/main.c
+++ week2/lab2/Lab2Part2G09/main.c
@@ -1,65 +1,99 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-/******************************************************************************
- * MSP432 Empty Project
- *
- * Description: An empty project that uses DriverLib
- *
- *                MSP432P401
- *             ------------------
- *         /|\|                  |
- *          | |                  |
- *          --|RST               |
- *            |                  |
- *            |                  |
- *            |                  |
- *            |                  |
- *            |                  |
- * Author: 
-*******************************************************************************/
 /* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
 /* Standard Includes */
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 
+/* Defines */
+#define LED BIT4
+#define B1 BIT0
+#define NUMBER (750000)
+
+
+uint8_t oldinterval=0x31; // default value for 1 sec
+uint16_t newinterval;
+const eUSCI_UART_ConfigV1 uartConfig=   // For CCSv9 and CCSv10 have UART_ConfigV1
+{
+ EUSCI_A_UART_CLOCKSOURCE_SMCLK,
+ 78,//BRDIV
+ 2, //UCxBRF
+ 0,//UCxBRS
+ EUSCI_A_UART_NO_PARITY,
+ EUSCI_A_UART_LSB_FIRST,
+ EUSCI_A_UART_ONE_STOP_BIT,
+ EUSCI_A_UART_MODE,
+ EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION
+
+};
+//Interrupt handler receives and echoes character typed in serial terminal even if it is out of the valid range 1-5 secs
+void EUSCIA0_IRQHandler(void)
+{
+    uint32_t status=UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
+    UART_clearInterruptFlag(EUSCI_A0_BASE,status);
+    if (status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+    {
+    newinterval=UART_receiveData(EUSCI_A0_BASE);
+    UART_transmitData(EUSCI_A0_BASE, newinterval);
+    UART_transmitData(EUSCI_A0_BASE, '\r');
+    UART_transmitData(EUSCI_A0_BASE, '\n');
+
+    }
+
+}
+char message[128];
+void putString(char message[])
+{
+    uint16_t i=0;
+
+    while (*(message+i)!='\0')
+    {
+        UART_transmitData(EUSCI_A0_BASE, *(message+i));
+    i++;
+    }
+    UART_transmitData(EUSCI_A0_BASE, 0x0D);
+    UART_transmitData(EUSCI_A0_BASE, 0x0A);
+
+}
+void delaysecs(uint8_t secs)
+{
+    uint32_t i;
+    for (i=0;i<NUMBER*secs;i++);
+    return;
+}
 int main(void)
 {
     /* Stop Watchdog  */
     MAP_WDT_A_holdTimer();
+    P6->DIR = LED; /* Set P6.0 as output. */
+    P6->REN = B1;   /* Enable the pull-up/down register for P6.1. */
+    P6->OUT = B1;   /* The pull-up/down register is set as pull-up. */
+
+
+    // P1.2 and P1.3 are UART TXD and RXD. These pins must be put in special function mode
+        GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,GPIO_PIN2|GPIO_PIN3,GPIO_PRIMARY_MODULE_FUNCTION);
+        //Sets DCO frequency at 12 MHz to get correct baud rate of 9600
+        CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
+        MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+        MAP_UART_enableModule(EUSCI_A0_BASE);
+        UART_enableInterrupt(EUSCI_A0_BASE,EUSCI_A_UART_RECEIVE_INTERRUPT);
+        Interrupt_enableInterrupt(INT_EUSCIA0);
+        Interrupt_enableMaster();
+
 
     while(1)
     {
-        
+        /* Make sure interval is between 1 and 5 */
+        if ( (newinterval > 0x30) & (newinterval < 0x36)) {
+            oldinterval = newinterval; /* If so save it as old interval */
+            sprintf(message, "LED FLASHING WITH %d SECS INTERVAL", oldinterval - 0x30);
+            putString(message);
+            newinterval = 0x00;
+        }
+        delaysecs(oldinterval - 0x30); /* Delay for oldinterval seconds */
+        if (!(P6->IN & B1) == 0) {
+            GPIO_toggleOutputOnPin (GPIO_PORT_P6, GPIO_PIN4); /* Toggle P6.4 */
+        }
     }
 }
