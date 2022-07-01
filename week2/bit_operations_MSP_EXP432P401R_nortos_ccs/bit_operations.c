e66a936682981ac0d54277d90d372e3869315140
= <=>
2022-07-01 13:59:34 -0400

    added more week2 files

---
+++ week2/bit_operations_MSP_EXP432P401R_nortos_ccs/bit_operations.c
@@ -0,0 +1,36 @@
+/* Functions that implement bit-level operations
+ *
+ */
+
+#include <stdint.h>
+#include "bit_operations.h"
+
+uint8_t
+countBits (uint8_t v)
+{
+    uint8_t count = 0;
+
+    /* FIXME: Write your code here. */
+
+    return count;
+}
+
+int
+isPowerOfTwo (uint8_t v)
+{
+    int status = 0;
+
+    /* FIXME: Write your code here. */
+
+    return status;
+}
+
+uint8_t
+rearrangeBits (uint8_t v)
+{
+    uint8_t rv = 0;
+
+    /* FIXME: Write your code here. */
+
+    return rv;
+}
