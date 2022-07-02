081e7be3efb0e1bea509ead1dbcc148bbd3f1ebf
ColeBardin <cab572@drexel.edu>
2022-07-01 20:20:54 -0400

    first attempt for countBits

--- week2/lab2/Lab2Part3G09/bit_operations.c
+++ week2/lab2/Lab2Part3G09/bit_operations.c
@@ -9,6 +9,17 @@ uint8_t countBits (uint8_t v)
 {
     uint8_t count = 0;
 
+    uint8_t copy;
+    uint8_t index;
+    uint8_t mask;
+
+    for (index=0; index<8; index++) {
+        mask = 1;
+        copy = (mask << index)^v;
+        if (copy < v) {
+            count++;
+        }
+    }
     /* FIXME: Write your code here. */
 
     return count;
