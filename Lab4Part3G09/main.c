3e638c1234f08896e5b24a2dc93b209fd1951b6c
ColeBardin <cab572@drexel.edu>
2022-07-13 11:23:37 -0400

    added l4p3 project file

---
+++ Lab4Part3G09/main.c
@@ -0,0 +1,19 @@
+/* Lab4Part3G09 */
+
+/* DriverLib Includes */
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    /* Stop Watchdog  */
+    MAP_WDT_A_holdTimer();
+
+    while(1)
+    {
+        
+    }
+}
