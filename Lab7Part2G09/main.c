72030eadf0aaffd97044fa19d0c273d058466b7a
ColeBardin <cab572@drexel.edu>
2022-08-03 11:05:40 -0400

    added lab7 empty projects

---
+++ Lab7Part2G09/main.c
@@ -0,0 +1,18 @@
+/* Lab 7 Part 2 G09 */
+/* DriverLib Includes */
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    /* Stop Watchdog  */
+    MAP_WDT_A_holdTimer();
+
+    while(1)
+    {
+        
+    }
+}
