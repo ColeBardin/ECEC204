5dc878583898460e91c3737f18484679e549ea13
ColeBardin <cab572@drexel.edu>
2022-08-04 14:54:20 -0400

    added started code for l7p2 and l7p3

--- Lab7Part2G09/main.c
+++ Lab7Part2G09/main.c
@@ -1,18 +1,122 @@
 /* Lab 7 Part 2 G09 */
-/* DriverLib Includes */
+/* DriverLib includes. */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-/* Standard Includes */
+/* Standard includes. */
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-int main(void)
-{
-    /* Stop Watchdog  */
-    MAP_WDT_A_holdTimer();
+/* User-specific includes. */
+#include "uart_functions.h"
 
-    while(1)
-    {
-        
+/* Defines. */
+#define PWM_PERIOD 32000 // Timer period
+#define DUTY_CYCLE_LENGTH 0.1 * PWM_PERIOD // Initial duty cycle length
+
+/* Configuration parameters for Timer_A module in PWM configuration */
+Timer_A_PWMConfig pwmConfig =
+{
+ TIMER_A_CLOCKSOURCE_ACLK,                  // Clock source
+ TIMER_A_CLOCKSOURCE_DIVIDER_1,             // Divider ratio, 1:1
+ PWM_PERIOD,                                // Timer period which will stored in CCR0
+ TIMER_A_CAPTURECOMPARE_REGISTER_1,         // Compare register to use
+ TIMER_A_OUTPUTMODE_RESET_SET,              // Output mode for PWM signal
+ DUTY_CYCLE_LENGTH                          // Duty cycle length which will be stored in CCR1
+};
+
+int
+main (void)
+{
+    /* Stop watchdog timer. */
+    MAP_WDT_A_holdTimer ();
+
+    /* Initialize the high-speed clock system. */
+    CS_setDCOCenteredFrequency (CS_DCO_FREQUENCY_12); // Set DCO to 12 MHz
+    CS_initClockSignal (CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    CS_initClockSignal (CS_HSMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    CS_initClockSignal (CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+
+    /* Initialize the low-speed auxiliary clock system. */
+    CS_setReferenceOscillatorFrequency (CS_REFO_32KHZ); // Reference oscillator is set to 32KHz
+    CS_initClockSignal (CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1); // Auxiliary clock derives from reference
+
+    /* Initialize the UART module. */
+    initUART ();
+    writeString ("Established communication with the board");
+
+    /* Configure output LED. */
+    GPIO_setAsOutputPin (GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setOutputLowOnPin (GPIO_PORT_P1, GPIO_PIN0);
+
+    /* Configure switch S1. */
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+
+    /* Configure P2.4 as the pin that outputs the PWM signal. */
+    GPIO_setAsPeripheralModuleFunctionOutputPin (GPIO_PORT_P2, GPIO_PIN4, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configure PWM. */
+    Timer_A_generatePWM (TIMER_A0_BASE, &pwmConfig);
+
+    /* Configure and enable interrupt system. */
+    GPIO_clearInterruptFlag (GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_enableInterrupt (GPIO_PORT_P1, GPIO_PIN1);
+    Interrupt_enableInterrupt (INT_PORT1); // ISR for when button S1 is pressed
+
+    Timer_A_enableCaptureCompareInterrupt(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
+    Timer_A_enableCaptureCompareInterrupt(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_1);
+    Interrupt_enableInterrupt (INT_TA0_0);
+    Interrupt_enableInterrupt (INT_TA0_N);
+    Interrupt_enableMaster ();
+
+    while (1);
+
+}
+
+/* ISR to handle switch S1. */
+void
+PORT1_IRQHandler (void)
+{
+    uint32_t status;
+    static float duty_cycle = 0.1;
+
+    status = GPIO_getEnabledInterruptStatus (GPIO_PORT_P1);
+    GPIO_clearInterruptFlag (GPIO_PORT_P1, status);
+
+    /* Isolate P1.1 */
+    if (status & GPIO_PIN1) {
+        if (duty_cycle >= 1.0)
+            duty_cycle = 0.1;
+        else
+            duty_cycle += 0.1;
+
+        /* Update the configuration with the length of the new duty cycle. */
+        int duty_cycle_length = (int) PWM_PERIOD * duty_cycle;
+        pwmConfig.dutyCycle = duty_cycle_length;
+        Timer_A_generatePWM (TIMER_A0_BASE, &pwmConfig);
     }
+
+    return;
+}
+
+/* ISR to handle end of timer period. */
+void
+TA0_0_IRQHandler (void)
+{
+    Timer_A_clearCaptureCompareInterrupt (TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
+    writeString ("\n\rEnd of timer period");
+    /* User-defined code. */
+
+    return;
+}
+
+/* ISR to handle end of PWM pulse. */
+void
+TA0_N_IRQHandler (void)
+{
+    Timer_A_clearCaptureCompareInterrupt (TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_1);
+    writeString ("\n\rEnd of PWM pulse");
+    /* User-defined code. */
+
+    return;
 }
--- Lab7Part3G09/main.c
+++ Lab7Part3G09/main.c
@@ -6,13 +6,73 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+const Timer_A_ContinuousModeConfig continuousModeConfig=
+{
+ TIMER_A_CLOCKSOURCE_ACLK,
+ TIMER_A_CLOCKSOURCE_DIVIDER_1,
+ TIMER_A_TAIE_INTERRUPT_DISABLE,
+ TIMER_A_DO_CLEAR
+
+};
+
+const Timer_A_CaptureModeConfig captureModeConfig=
+{
+ TIMER_A_CAPTURECOMPARE_REGISTER_2,
+ TIMER_A_CAPTUREMODE_RISING_EDGE,
+ TIMER_A_CAPTURE_INPUTSELECT_CCIxA,
+ TIMER_A_CAPTURE_SYNCHRONOUS,
+ TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE,
+ TIMER_A_OUTPUTMODE_OUTBITVALUE
+};
+
+uint8_t i=0;
+uint32_t j=0;
+float Timeint;
+
+volatile uint16_t CaptureValues[10]={0,0,0,0,0,0,0,0,0,0};
+//volatile float CaptureFreq[10];
+
 int main(void)
 {
-    /* Stop Watchdog  */
     MAP_WDT_A_holdTimer();
+
+    CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
+    CS_initClockSignal(CS_ACLK,CS_REFOCLK_SELECT,CS_CLOCK_DIVIDER_64);
+
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P2, GPIO_PIN5,GPIO_PRIMARY_MODULE_FUNCTION);
+
+    Timer_A_configureContinuousMode(TIMER_A0_BASE, &continuousModeConfig);
+    Timer_A_initCapture(TIMER_A0_BASE, &captureModeConfig);
+
+
+    Timer_A_enableCaptureCompareInterrupt (TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_2);
+    Interrupt_enableInterrupt(INT_TA0_N);
+    Interrupt_enableMaster();
+    Timer_A_clearCaptureCompareInterrupt(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_2);
+    Timer_A_startCounter(TIMER_A0_BASE,TIMER_A_CONTINUOUS_MODE);
+
+    GPIO_setAsOutputPin(GPIO_PORT_P2,GPIO_PIN1);
+    GPIO_setDriveStrengthHigh(GPIO_PORT_P2,GPIO_PIN1);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2,GPIO_PIN1);
+
+
 
     while(1)
     {
-        
+       j++;
+       j--;
+
     }
 }
+
+void TA0_N_IRQHandler(void)
+{
+   GPIO_toggleOutputOnPin(GPIO_PORT_P2,GPIO_PIN1);
+   CaptureValues[i]=Timer_A_getCaptureCompareCount(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_2);
+
+   {   if(i>0)
+       Timeint=(float)(CaptureValues[i]-CaptureValues[i-1])/2000.0f;
+   i++;
+   }
+   Timer_A_clearCaptureCompareInterrupt(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_2);
+ }
