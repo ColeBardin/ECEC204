672ac62480c02b5f30b02d0c8b47ca7afb712043
jmath3912 <51333711+jmath3912@users.noreply.github.com>
2022-08-12 12:18:08 -0400

    Added Lab8Part1 code

--- Lab8Part1G09/main.c
+++ Lab8Part1G09/main.c
@@ -1,4 +1,7 @@
-/* Lab 8 Part 1 by G09 */
+/* Description: Skeleton code for the program monitor
+ *
+ * Author: Naga Kandasamy
+*******************************************************************************/
 /* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
@@ -10,67 +13,32 @@
 #include "uart_functions.h"
 #include "blur_filter.h"
 
-
-const Timer_A_ContinuousModeConfig continuousModeConfig=
-    {
-     TIMER_A_CLOCKSOURCE_ACLK,
-     TIMER_A_CLOCKSOURCE_DIVIDER_1,
-     TIMER_A_TAIE_INTERRUPT_ENABLE,
-     TIMER_A_DO_CLEAR
-
-    };
-uint16_t time1=0;
-uint16_t time2=0;
-uint16_t ovf=0;
-uint16_t cnt=0;
-
-float timeint=0.0f;
 uint32_t M;
 /* Global variables containing the input and output images. */
 image_t in, out;
 
-int
-main (void)
-{
+int main (void) {
     /* Stop Watchdog  */
     MAP_WDT_A_holdTimer ();
-    // Set up LFXT and from that derive ACLK at 32 KHz
-    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,GPIO_PIN0|GPIO_PIN1,GPIO_PRIMARY_MODULE_FUNCTION);
-    CS_setExternalClockSourceFrequency(32768,48000000);
-    CS_initClockSignal(CS_ACLK,CS_LFXTCLK_SELECT,CS_CLOCK_DIVIDER_1);
+    GPIO_setAsOutputPin(GPIO_PORT_P2,GPIO_PIN0|GPIO_PIN1);
+    GPIO_setDriveStrengthHigh(GPIO_PORT_P2,GPIO_PIN0|GPIO_PIN1);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P2,GPIO_PIN0);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2,GPIO_PIN1);
 
-    // Set up DCOCLK at 12 MHz and from that derive CPU clock MCLK at 12 MHz
     CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
-    CS_initClockSignal(CS_MCLK,CS_DCOCLK_SELECT,CS_CLOCK_DIVIDER_1);
+    CS_initClockSignal(CS_MCLK,CS_DCOCLK_SELECT,CS_CLOCK_DIVIDER_2);
     M=CS_getMCLK();
 
-    // Configure Timer A1 in continuous mode and start it, clear overflow
-    Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig);
-    Interrupt_enableInterrupt(INT_TA1_N);
-    Interrupt_enableMaster();
-    Timer_A_startCounter(TIMER_A1_BASE,TIMER_A_CONTINUOUS_MODE);
-    ovf=0;
+    CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);// REFOCLK is 32 KHz
+    CS_initClockSignal(CS_ACLK,CS_REFOCLK_SELECT,CS_CLOCK_DIVIDER_2);// ACLK is 8192 Hz
+    SysCtl_setWDTTimeoutResetType(SYSCTL_SOFT_RESET);
+    WDT_A_initWatchdogTimer(WDT_A_CLOCKSOURCE_ACLK,WDT_A_CLOCKITERATIONS_32K);//Watchdog reset at 1 sec
+    WDT_A_startTimer();
 
-    // Take timer reading before starting to execute image processing
-    time1= Timer_A_getCounterValue(TIMER_A1_BASE);
     acquireImage (&in, MIN_VALUE, MAX_VALUE, SIZE); // Acquire image
     blurFilter (&in, &out); // Process image
-    // Take timer reading after completing image processing execution
-    time2= Timer_A_getCounterValue(TIMER_A1_BASE);
 
-    // Calculate elapsed time
-    cnt=ovf;
-    timeint = ((float)(time2 - time1)+65536.0f*(float)cnt)/32768.0f;
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2,GPIO_PIN0);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P2,GPIO_PIN1);
 
-    while(1);
-}
-
-// Interrupt Handler to count overflows
-void TA1_N_IRQHandler(void)
-{
-    Timer_A_clearInterruptFlag(TIMER_A1_BASE);
-    ovf++;
-}
-
-
-
+    }
