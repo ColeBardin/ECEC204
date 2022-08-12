48e9770bb3c7bc0a39051152925263feacc6c40b
ColeBardin <cab572@drexel.edu>
2022-08-12 09:18:36 -0400

    added skeleton code to l8p1

--- Lab8Part1G09/main.c
+++ Lab8Part1G09/main.c
@@ -1,51 +1,4 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-/******************************************************************************
- * MSP432 Empty Project
- *
- * Description: An empty project that uses DriverLib
- *
- *                MSP432P401
- *             ------------------
- *         /|\|                  |
- *          | |                  |
- *          --|RST               |
- *            |                  |
- *            |                  |
- *            |                  |
- *            |                  |
- *            |                  |
- * Author: 
-*******************************************************************************/
+/* Lab 8 Part 1 by G09 */
 /* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
@@ -53,13 +6,71 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-int main(void)
+/* User-specific includes. */
+#include "uart_functions.h"
+#include "blur_filter.h"
+
+
+const Timer_A_ContinuousModeConfig continuousModeConfig=
+    {
+     TIMER_A_CLOCKSOURCE_ACLK,
+     TIMER_A_CLOCKSOURCE_DIVIDER_1,
+     TIMER_A_TAIE_INTERRUPT_ENABLE,
+     TIMER_A_DO_CLEAR
+
+    };
+uint16_t time1=0;
+uint16_t time2=0;
+uint16_t ovf=0;
+uint16_t cnt=0;
+
+float timeint=0.0f;
+uint32_t M;
+/* Global variables containing the input and output images. */
+image_t in, out;
+
+int
+main (void)
 {
     /* Stop Watchdog  */
-    MAP_WDT_A_holdTimer();
+    MAP_WDT_A_holdTimer ();
+    // Set up LFXT and from that derive ACLK at 32 KHz
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,GPIO_PIN0|GPIO_PIN1,GPIO_PRIMARY_MODULE_FUNCTION);
+    CS_setExternalClockSourceFrequency(32768,48000000);
+    CS_initClockSignal(CS_ACLK,CS_LFXTCLK_SELECT,CS_CLOCK_DIVIDER_1);
 
-    while(1)
-    {
-        
-    }
+    // Set up DCOCLK at 12 MHz and from that derive CPU clock MCLK at 12 MHz
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
+    CS_initClockSignal(CS_MCLK,CS_DCOCLK_SELECT,CS_CLOCK_DIVIDER_1);
+    M=CS_getMCLK();
+
+    // Configure Timer A1 in continuous mode and start it, clear overflow
+    Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig);
+    Interrupt_enableInterrupt(INT_TA1_N);
+    Interrupt_enableMaster();
+    Timer_A_startCounter(TIMER_A1_BASE,TIMER_A_CONTINUOUS_MODE);
+    ovf=0;
+
+    // Take timer reading before starting to execute image processing
+    time1= Timer_A_getCounterValue(TIMER_A1_BASE);
+    acquireImage (&in, MIN_VALUE, MAX_VALUE, SIZE); // Acquire image
+    blurFilter (&in, &out); // Process image
+    // Take timer reading after completing image processing execution
+    time2= Timer_A_getCounterValue(TIMER_A1_BASE);
+
+    // Calculate elapsed time
+    cnt=ovf;
+    timeint = ((float)(time2 - time1)+65536.0f*(float)cnt)/32768.0f;
+
+    while(1);
 }
+
+// Interrupt Handler to count overflows
+void TA1_N_IRQHandler(void)
+{
+    Timer_A_clearInterruptFlag(TIMER_A1_BASE);
+    ovf++;
+}
+
+
+
