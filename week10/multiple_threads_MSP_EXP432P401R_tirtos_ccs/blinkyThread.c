cad264f22a0baf4d455796b4f43c8e13b62267c1
ColeBardin <cab572@drexel.edu>
2022-08-24 13:52:46 -0400

    added week10 material

---
+++ week10/multiple_threads_MSP_EXP432P401R_tirtos_ccs/blinkyThread.c
@@ -0,0 +1,25 @@
+/* Description: Thread that toggles LED1 at specified rate.
+ */
+
+#include <unistd.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <ti/drivers/GPIO.h>
+#include "Board.h"
+
+void *blinkyThread(void *arg0)
+{
+    int delay = (int)arg0;
+
+    /* Call driver init functions */
+    GPIO_init();
+
+    /* Configure the LED pin and turn on LED1  */
+    GPIO_setConfig(Board_GPIO_LED0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
+    GPIO_write(Board_GPIO_LED0, Board_GPIO_LED_ON);
+
+    while (1) {
+        sleep(delay);
+        GPIO_toggle(Board_GPIO_LED0);
+    }
+}
---
+++ week10/multiple_threads_MSP_EXP432P401R_tirtos_ccs/uartThread.c
@@ -0,0 +1,45 @@
+/* Description: Thread to interact with the terminal via the UART.
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+#include <ti/drivers/GPIO.h>
+#include <ti/drivers/UART.h>
+#include "Board.h"
+
+void *uartThread(void *arg0)
+{
+    char        input;
+    const char  echoPrompt[] = "Echoing characters:\r\n";
+    UART_Handle uart;
+    UART_Params uartParams;
+
+    /* Call driver init functions */
+    UART_init();
+
+    /* Create a UART with data processing off */
+    UART_Params_init(&uartParams);
+    uartParams.writeDataMode = UART_DATA_BINARY;
+    uartParams.readDataMode = UART_DATA_BINARY;
+    uartParams.readReturnMode = UART_RETURN_FULL;
+    uartParams.readEcho = UART_ECHO_OFF;
+    uartParams.baudRate = 115200;
+
+    uart = UART_open(Board_UART0, &uartParams);
+
+    if (uart == NULL) {
+        /* UART_open() failed */
+        while (1);
+    }
+
+    UART_write(uart, echoPrompt, sizeof(echoPrompt));
+
+    /* Loop forever echoing input received from the UART */
+    while (1) {
+        UART_read(uart, &input, 1);
+        UART_write(uart, &input, 1);
+    }
+}
+
+
+
